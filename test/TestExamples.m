72ea3f5973232fe5dc4210450393d2f411183bb9
ebenetce <ebenetce@uk-ebenetce-y.dhcp.mathworks.com>
2021-02-22 08:58:02 +0000

    intentional error

--- test/TestExamples.m
+++ test/TestExamples.m
@@ -23,7 +23,7 @@ classdef TestExamples < matlab.unittest.TestCase
 
             % Calculate expected result
             dt = datetime(dateStr,"Format","MM/dd/uuuu");
-            doyExpected = day(dt,"dayofyear");
+            doyExpected = day(dt,"dayofyear") + 1 ;
 
             % Get actual result
             doyActual = dayofyear(dateStr);
